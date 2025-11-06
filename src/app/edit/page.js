12169e8f31b35bfd345abad4231c9bad42491a8c
Sachin Bhujel <90704616+sachinbhujel@users.noreply.github.com>
2025-11-06 16:03:15 +0000

    UI enhance for all devices

--- src/app/edit/page.js
+++ src/app/edit/page.js
@@ -843,12 +843,9 @@ export default function Edit() {
                                 ))}
                         </div>
                     ) : (
-                        <div
-                            className="w-[95%] h-[95%] border-2 bg-white rounded-md border-dashed p-4
-             flex items-center justify-center flex-col gap-3 cursor-pointer overflow-auto upload"
-                        >
+                        <div className="w-[95%] h-[95%] border-2 bg-white rounded-md border-dashed cursor-pointer overflow-auto upload">
                             <div className="relative flex flex-col justify-center items-center h-[100dvh]">
-                                <label className="flex flex-col justify-center items-center gap-2">
+                                <label className="w-[100%] h-[100%] bg-red-200 cursor-pointer flex flex-col justify-center items-center gap-2">
                                     <div className="flex flex-col justify-center items-center">
                                         <svg
                                             xmlns="http://www.w3.org/2000/svg"
@@ -891,12 +888,13 @@ export default function Edit() {
                                             viewBox="0 0 24 24"
                                             fill="none"
                                             stroke="currentColor"
-                                            strokeWidth="2"
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            className="lucide lucide-chevron-down-icon lucide-chevron-down"
+                                            stroke-width="2"
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            class="lucide lucide-move-right-icon lucide-move-right"
                                         >
-                                            <path d="m6 9 6 6 6-6" />
+                                            <path d="M18 8L22 12L18 16" />
+                                            <path d="M2 12H22" />
                                         </svg>
                                     </div>
                                 </label>
